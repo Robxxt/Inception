e7cb91a3f9b4e01f655a727922e1392673710578
Robxxt <rdragan@student.42heilbronn.de>
2024-02-03 11:40:45 +0100

    fix problems

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -4,15 +4,14 @@ mkdir -p /var/www/html && cd /var/www/html
 rm -rf *
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
 
-chmod +x wp-cli.phar 
+chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 wp core download --allow-root
 
-sed -i "s/define( 'DB_NAME', 'db1' );/define( 'DB_NAME', '$DB_NAME' );/" wp-config.php
-sed -i "s/define( 'DB_USER', 'user' );/define( 'DB_USER', '$DB_USER' );/" wp-config.php
-sed -i "s/define( 'DB_PASSWORD', 'pwd' );/define( 'DB_PASSWORD', '$DB_PASSWORD' );/" wp-config.php
+sed -i "s/define( 'DB_NAME', 'db1' );/define( 'DB_NAME', '$DB_NAME' );/" /wp-config.php
+sed -i "s/define( 'DB_USER', 'user' );/define( 'DB_USER', '$DB_USER' );/" /wp-config.php
+sed -i "s/define( 'DB_PASSWORD', 'pwd' );/define( 'DB_PASSWORD', '$DB_PASSWORD' );/" /wp-config.php
 
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 mv /wp-config.php /var/www/html/wp-config.php
 
 wp core install --url=$AUTHOR.$DOMAIN/ --title=$WP_TITLE --admin_user=$AUTHOR --admin_password=$DB_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
