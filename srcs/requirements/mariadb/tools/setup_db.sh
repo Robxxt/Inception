7956e17492bbc48935e24f64dd99762705635d8c
Robxxt <rdragan@student.42heilbronn.de>
2024-02-03 11:48:49 +0100

    fix database errors

--- srcs/requirements/mariadb/tools/setup_db.sh
+++ srcs/requirements/mariadb/tools/setup_db.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-service mariadb start 
+# Start MariaDB service
+service mariadb start
 
+# Wait for MariaDB to start
+sleep 5
+
+# Create SQL script
 echo "CREATE DATABASE IF NOT EXISTS $DB_NAME ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
+# Execute SQL script
 mysql < db1.sql
 
+# Stop MariaDB service
 service mariadb stop
 
+# Start mysqld
 mysqld
