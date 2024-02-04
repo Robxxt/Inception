a8f1880526401922d79420f9b935961de321788e
Robxxt <rdragan@student.42heilbronn.de>
2024-02-04 15:44:16 +0100

    change typo

--- srcs/requirements/nginx/tools/script.sh
+++ srcs/requirements/nginx/tools/script.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-mkdir -p $CERT_PATH
+mkdir -p "$CERT_PATH"
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $CERT_PATH/$AUTHOR.key \
-    -out $CERT_PATH/$AUTHOR.crt -subj "/C=DE/ST=Baden-Württemberg/L=Heilbronn/O=42Heilbronn/OU=42Heilbronnal Unit/CN=Common Name"
-
-# chown -R www-data:www-data /var/www/html
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout "$CERT_PATH/$AUTHOR.key" \
+    -out "$CERT_PATH/$AUTHOR.crt" -subj "/C=DE/ST=Baden-Württemberg/L=Heilbronn/O=42Heilbronn/OU=42Heilbronn Unit/CN=Common Name"
 
 cat <<EOF > /etc/nginx/nginx.conf
 worker_processes auto;
@@ -23,8 +21,8 @@ http {
         index index.php index.html;
         server_name $AUTHOR.$DOMAIN;
 
-        ssl_certificate $CERT_PATH/$AUTHOR.crt;
-        ssl_certificate_key $CERT_PATH/$AUTHOR.key;
+        ssl_certificate "$CERT_PATH/$AUTHOR.crt";
+        ssl_certificate_key "$CERT_PATH/$AUTHOR.key";
 
         ssl_protocols TLSv1.2 TLSv1.3;
         ssl_ciphers 'TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384';
@@ -42,12 +40,13 @@ http {
             etag off;
         }
         location ~ \.php$ {
-            fastcgi_split_path_info ^(.+\.php)(/.+)$;
+            include snippets/fastcgi-php.conf;
             fastcgi_pass wordpress:9000;
-            fastcgi_index index.php;
-            include fastcgi_params;
             fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
             fastcgi_param PATH_INFO \$fastcgi_path_info;
+            fastcgi_buffer_size 128k;
+            fastcgi_buffers 4 256k;
+            fastcgi_busy_buffers_size 256k;
         }
     }
 }
